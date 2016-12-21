4c37d3b5a8ea0cdf83cce075932e92250e499b77
Amandeep <amandeep.teastallstudio@gmail.com>
2016-12-21 12:18:18 +0530

    Declare new variable

--- app/src/main/java/amandeep/com/materialdesign/MainActivity.java
+++ app/src/main/java/amandeep/com/materialdesign/MainActivity.java
@@ -1,9 +1,11 @@
 package amandeep.com.materialdesign;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button brn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
